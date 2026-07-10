4387d6cbe6a5e29d10b3567a15962d0becb46065
Brian <epitome-tampers.1v@icloud.com>
2026-07-10 09:03:54 -0700

    accumulated changes

--- compressdeletelogs.sh
+++ compressdeletelogs.sh
@@ -5,9 +5,10 @@ set -o errtrace  # trace ERR through 'time command' and other functions
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # http://stackoverflow.com/questions/59895
+cd "$DIR"
 
-read -r -n 1 -p "Is this a good question (Y/n)? " answer; echo
-if [[ "${answer:-Y}" =~ [yY] ]]; then echo Yes; else echo No; exit; fi
+# compress files older than a day
+find ./prompt_logs/ -type f -mtime +0  -regextype posix-extended -regex ".*/[0-9]{12}\.txt" -exec zstd --rm -9 {} +
 
-cd "$DIR"/src/
-cmake --install build --config Release
+# delete files older than a month
+find ./prompt_logs/ -type f -mtime +30 -regextype posix-extended -regex ".*/[0-9]{12}\.txt\.zst" -delete
